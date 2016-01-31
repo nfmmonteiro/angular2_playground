b89053ad414295fb92fe46db4df63097e3ff7b2f
Nuno Monteiro <nfmmonteiro@gmail.com>
2016-01-31 02:03:40 +0000

    Explores NgFor exported values (index, odd, even, last), as well as, basic component inline styling.

--- ng-book-2/manuscript/code/first_app/angular2-reddit-base/app.ts
+++ ng-book-2/manuscript/code/first_app/angular2-reddit-base/app.ts
@@ -4,12 +4,34 @@ import { Component } from 'angular2/core';
 @Component({
     selector: 'hello-world',
     template: `
-        <div>
             <h1>{{title}}</h1>
-            <ul>
-                <li *ngFor="#name of names">{{name}}</li>
-            </ul>
-        </div>`
+            <table>
+                <thead>
+                    <tr>
+                       <th>Name</th>
+                       <th>Index</th>
+                       <th>Odd</th>
+                       <th>Even</th>
+                       <th>Last</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr *ngFor="#name of names; #index=index; #odd=odd; #even=even; #last=last" class="{{odd ? 'odd' : ''}}">
+                        <td>{{name}}</td>
+                        <td>{{index}}</td>
+                        <td>{{odd}}</td>
+                        <td>{{even}}</td>
+                        <td>{{last}}</td>
+                    </tr>
+                </tbody>
+            </table>
+        `,
+    styles: [`
+        table { width: 100%; }
+        thead { text-align: left; background: LightBlue; }
+        th,td { border: 1px solid grey; padding: 5px; }
+        .odd { background-color: rgba(188, 198, 204, 0.16); }
+    `]
 })
 class HelloWorld {
     private title:string;
